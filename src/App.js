d4af7f900da10a965bf32c720377372cad04b218
Viktor Strelianyi <chmpain@gmail.com>
2021-10-05 16:13:19 +0300

    -test 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header'
-import MainBlock from './components/MainBlock/MainBlock';
-import Modal from './components/Modal/Modal';
-import ModalLast from './components/ModalLast/ModalLast';
-import './App.scss';
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import MainBlock from "./components/MainBlock/MainBlock";
+import Modal from "./components/Modal/Modal";
+import ModalLast from "./components/ModalLast/ModalLast";
+import "./App.scss";
 
-const URL = 'https://arbcrm.site/rest/quiz/view?id=2';
+const URL = "https://arbcrm.site/rest/quiz/view?id=2";
 
 // Список для доработки:
 // - Вычиститть мусор от разработчикаЖ подсказки, комментарии...
@@ -31,18 +31,15 @@ class App extends React.Component {
     };
   }
 
-  onNextButtonClick = () => {    
-    this.setState(
-      {
-        currentQuestion: this.state.currentQuestion + 1,
-      }
-    );
-  }  
+  onNextButtonClick = () => {
+    this.setState({
+      currentQuestion: this.state.currentQuestion + 1
+    });
+  };
 
   componentDidMount() {
-
     fetch(URL)
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
         (result) => {
           this.setState({
@@ -56,11 +53,10 @@ class App extends React.Component {
             error
           });
         }
-      )
+      );
   }
 
   render() {
-
     // const { error, isLoaded, quiz } = this.state;
     // if (error) {
     //   return <div>Ошибка: {error.message}</div>;
@@ -72,36 +68,34 @@ class App extends React.Component {
       <BrowserRouter>
         <div className="page-wrapper">
           <Header />
-          <main className='app-wrapper-content'>
+          <main className="app-wrapper-content">
             <Route
               exact
-              path='/'
-              render={() => (
-                <MainBlock data={this.state.data} />
-              )}
+              path="/"
+              render={() => <MainBlock data={this.state.data} />}
             />
 
             <Route
               exact
-              path='/modal'
-              render={
-                (props) => {
-                  return this.state.data.id && <Modal
-                    data={this.state.data}
-                    currentQuestion={this.state.currentQuestion}
-                    questionsNumbers={this.state.questionsNumbers}
-                    onNextButtonClick={this.onNextButtonClick}
-                  />
-                }
-              }
+              path="/modal"
+              render={(props) => {
+                return (
+                  this.state.data.id && (
+                    <Modal
+                      data={this.state.data}
+                      currentQuestion={this.state.currentQuestion}
+                      questionsNumbers={this.state.questionsNumbers}
+                      onNextButtonClick={this.onNextButtonClick}
+                    />
+                  )
+                );
+              }}
             />
 
             <Route
               exact
-              path='/modalLast'
-              render={(props) => (
-                <ModalLast data={this.state.data} />
-              )}
+              path="/modalLast"
+              render={(props) => <ModalLast data={this.state.data} />}
             />
           </main>
           <Footer />
